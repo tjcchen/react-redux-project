1cc2b0c2acd5356b83575df21f80e8995400fb44
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-08 12:18:11 +0800

    feat: add scaffolding for react redux project

---
+++ src/TodoApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function TodoApp() {
+  return (
+    <div className="todo-app">
+      <h2>Todo List</h2>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path                   = require('path');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: path.join(__dirname, 'public/index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
+      inject: true,
+      minify: {
+        html5: true,
+        collapseWhitespace: false,
+        preserveLineBreaks: true,
+        minifyCSS: false,
+        minifyJS: false,
+        removeComments: true
+      }
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
