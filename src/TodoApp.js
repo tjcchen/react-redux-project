54812b50fffc0f09a78afcfde6a2801da2f6022d
Andy Chen <chenyangpoppin@hotmail.com>
2020-11-08 16:25:01 +0800

    feat: add react redux code logic phase 1

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddTodo from './components/AddTodo';
 import './index.css';
 
 export default function TodoApp() {
   return (
     <div className="todo-app">
       <h2>Todo List</h2>
+      <AddTodo />
     </div>
   );
 }
